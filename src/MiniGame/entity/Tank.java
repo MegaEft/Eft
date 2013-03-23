65c14e9207d9c61a18218fa2b8554263bda868e1
Josh Becker the 3rd <aswellkindaguy@gmail.com>
2013-03-22 22:21:20 -0500

    Added the ability for the tank to create treads and got ride of some of the hard code

--- src/MiniGame/entity/Tank.java
+++ src/MiniGame/entity/Tank.java
@@ -6,6 +6,8 @@ package MiniGame.entity;
 
 import MiniGame.Camera;
 import MiniGame.world.World;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.lwjgl.util.vector.Vector3f;
 import org.newdawn.slick.*;
 import org.newdawn.slick.geom.Polygon;
@@ -17,19 +19,18 @@ import org.newdawn.slick.geom.Polygon;
 public class Tank extends HealthEntity {
     private long currentLifeTimeMS = 0;
     private long lastShotFiredTimeMS = 0;
-    private Image upperTank= new Image("res/actors/upperTank.png");
-    private Image lowerTank= new Image("res/actors/lowerTank.png");
-    private Input userInput;
+    private long lastTreadCreated = 0; 
+    private Image upperTank = new Image("res/actors/upperTank.png");
+    private Image lowerTank = new Image("res/actors/lowerTank.png");
+    private Image im = new Image("res/actors/treads.png");
     private double angleToTurn;
     private float tankX,tankY,xDist,yDist, mouseX,mouseY;
     private String mousex,mousey,ang,s_tankX,s_tankY;
-    
     
     @Override
     public boolean update(GameContainer slickContainer, int deltaMS){
         try{
             currentLifeTimeMS += deltaMS;
-            Input input = slickContainer.getInput();
             this.setInput(slickContainer.getInput());
             this.direction();
             this.tankMovement(slickContainer.getInput(), deltaMS);
@@ -48,14 +49,21 @@ public class Tank extends HealthEntity {
         g.draw(this.entityPoly);
     }
     
-    public Tank(World world, float x, float z) throws SlickException {
+    public Tank(World world, float x, float y, float z) throws SlickException {
         super(world, x, z);
-        upperTank.setCenterOfRotation(38,38);
-        lowerTank.setCenterOfRotation(49,32);
-        tankX=500;
-        tankY=300;
-        lowerTank.draw(tankX,tankY);
-        upperTank.draw(tankX,tankY);
+        upperTank.setCenterOfRotation(15,upperTank.getHeight()/2);
+        lowerTank.setCenterOfRotation(lowerTank.getWidth()/2,lowerTank.getHeight()/2);
+        
+        if(x<=-1 || x>1000){
+            tankX=500;
+        }else
+            tankX=x;
+        
+        if(y<=-1 || y>1000){
+            tankX=300;
+        }else
+            tankY=y;
+        
         this.entityPoly =  new Polygon(new float[]{tankX,tankY,tankX+lowerTank.getWidth(),tankY,tankX+lowerTank.getWidth(),tankY+lowerTank.getHeight(),tankX,tankY+lowerTank.getHeight()});
         updatePoly(tankX,tankY);
     }
@@ -72,8 +80,8 @@ public class Tank extends HealthEntity {
     }
     
     private void drawTank(){
-        lowerTank.draw(tankX-9,tankY+6);
-        upperTank.draw(tankX,tankY);
+        lowerTank.draw(tankX,tankY);
+        upperTank.draw(tankX+15,tankY+lowerTank.getCenterOfRotationY()/2);
         upperTank.setRotation((float)angleToTurn);
     }
     
@@ -86,7 +94,7 @@ public class Tank extends HealthEntity {
         return "Mouse X: "+mousex+"Mouse Y: "+mousey+" Angle: "+ang+"\nCurrent X: "+s_tankX+" Current Y: "+s_tankY;
     }
     
-    private void tankMovement(Input userI, int delta){
+    private void tankMovement(Input userI, int delta) throws SlickException{
         if(userI.isKeyDown(Input.KEY_LEFT)){
             lowerTank.rotate(-.15f*delta);
         }
@@ -102,7 +110,8 @@ public class Tank extends HealthEntity {
                     tankY+= hip * Math.cos(Math.toRadians(lowerTank.getRotation()));
                     tankX-= hip * Math.sin(Math.toRadians(lowerTank.getRotation()));
                     updatePoly(tankX,tankY);
-            }
+            }    
+            createTread(delta);
         }
         if(userI.isKeyDown(Input.KEY_DOWN)){
             float hip=-.15f*delta;
@@ -113,14 +122,14 @@ public class Tank extends HealthEntity {
                 tankY-= hip * Math.cos(Math.toRadians(lowerTank.getRotation()));
                 tankX+= hip * Math.sin(Math.toRadians(lowerTank.getRotation()));
                 updatePoly(tankX,tankY);
-            }            
+            }    
+            createTread(delta);
         }
         if(userI.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)){
             if (this.currentLifeTimeMS > this.lastShotFiredTimeMS + getFireDelay()) {
                 this.lastShotFiredTimeMS = this.currentLifeTimeMS;
                 Vector3f shootDir = new Vector3f((float) (Math.sin(Math.PI/180 * (angleToTurn+90))),(float) (Math.cos(Math.PI/180 * (angleToTurn+90))), 0);
                 shootBullet(tankX+38, -tankY-38, 0, shootDir);
-
             }
         }
     }
@@ -129,6 +138,8 @@ public class Tank extends HealthEntity {
         float bulletSpeed = 600;
         Projectile bullet = new Projectile(world, tx , tz , 0 , projectileDirection, bulletSpeed);
         world.addEntity(bullet);
+        
+        
     }
     
     public int getFireDelay() {
@@ -137,5 +148,18 @@ public class Tank extends HealthEntity {
             level = 3;
         }
         return 250 - (25 * level);
+    }
+    public void createTread(int delta){
+        if(this.currentLifeTimeMS>this.lastTreadCreated){
+            this.lastTreadCreated=50+this.currentLifeTimeMS;
+            try {
+                
+                TrailEntity trail = new TrailEntity(world,tankX,tankY,0f,im,delta,lowerTank.getRotation(),lowerTank.getCenterOfRotationX(),lowerTank.getCenterOfRotationY(),lowerTank.getWidth(),2500);
+                world.addEntity(trail);
+            } catch (SlickException ex) {
+                System.out.println("Tread Error "+ex);
+            }
+            
+        }
     }
 }
