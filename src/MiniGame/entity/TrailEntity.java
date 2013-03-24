e4e3ffb037551133aa23967c56c0b119f622a2bf
Josh Becker the 3rd <aswellkindaguy@gmail.com>
2013-03-24 14:38:10 -0500

    Added myself as an author

--- src/MiniGame/entity/TrailEntity.java
+++ src/MiniGame/entity/TrailEntity.java
@@ -4,11 +4,9 @@
  */
 package MiniGame.entity;
 import org.newdawn.slick.*;
-import org.newdawn.slick.geom.Vector2f;
 import MiniGame.world.*;
 import MiniGame.Camera;
-import MiniGame.util.OffsetImage;
-import org.lwjgl.util.vector.Vector3f;
+import org.newdawn.slick.Graphics;
 /**
  *
  * @author A Swell Kinda Guy
@@ -24,6 +22,7 @@ public class TrailEntity extends Entity {
     protected float distance;
     protected float centerOfSpawnObjX;
     protected float centerOfSpawnObjY;
+    protected Color fadeColor;
     /*
      *
      * @throws SlickException
@@ -31,6 +30,7 @@ public class TrailEntity extends Entity {
     protected TrailEntity(World world, float x, float y, float z,Image image,int deltaMS, float rotation,float objCenterX, float objCenterY,float distance,int lifespan) throws SlickException {
         super(world,x,z);
         this.endLifeTimeMs=lifespan+deltaMS;
+        
         this.trailX= x;
         this.trailY= y;
      
@@ -40,29 +40,27 @@ public class TrailEntity extends Entity {
         this.trail = image;
         this.distance=distance;
         this.direction=rotation;
+        this.fadeColor = new Color(1f,1f,1f,1f);
+        
         this.entityType = EntityType.TrailEntity;
-     
-    }   
-    
+    }
     @Override
     public boolean update(GameContainer gc,int deltaMS){
-        if( (currentLifeTimeMs+=deltaMS)>endLifeTimeMs )
+       
+       if( (currentLifeTimeMs+=deltaMS)>endLifeTimeMs )
+            this.fadeColor.a-=deltaMS*.0005f;
+       
+       if(fadeColor.a<=0)
             this.setRemoved();
         
         return true;
     }
     @Override
     public void render(GameContainer gc, Graphics g, Camera camera){
+       
         g.rotate(this.trailX+centerOfSpawnObjX, this.trailY+centerOfSpawnObjY, this.direction);
-        g.drawImage(this.trail, this.trailX, this.trailY);
-        g.drawImage(this.trail, this.trailX+distance-this.trail.getWidth(), this.trailY);
+        g.drawImage(this.trail, this.trailX, this.trailY,this.fadeColor);
+        g.drawImage(this.trail, this.trailX+distance-this.trail.getWidth(), this.trailY,this.fadeColor);
         g.rotate(this.trailX+centerOfSpawnObjX, this.trailY+centerOfSpawnObjY, -this.direction);
-/*
-        g.rotate(this.trailX2+c, this.trailY2+cc, this.direction);
-        
-        g.rotate(this.trailX2+c, this.trailY2+cc, -this.direction);
-    
-    */ }
-    
-    
+    }
 }
